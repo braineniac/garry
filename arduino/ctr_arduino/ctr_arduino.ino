5dadc215548d771b404ca35788a19a5a291ca14d
Daniel Hammer <braineniac@protonmail.com>
2018-10-23 22:39:55 +0200

    arduino functions done

--- arduino/ctr_arduino/ctr_arduino.ino
+++ arduino/ctr_arduino/ctr_arduino.ino
@@ -1,4 +1,5 @@
 #include <Wire.h>
+#include <Servo.h>
 
 #define SLAVE_ADDR 0x04
 
@@ -11,6 +12,9 @@
 #else
 #define LOG(x) // do nothing
 #endif
+
+Servo servoA;
+Servo servoB;
  
 void setup() {
 
@@ -32,7 +36,18 @@ void setup() {
     pinMode(13, OUTPUT); //Initiates Motor Channel A pin
     pinMode(8, OUTPUT);  //Initiates Brake Channel A pin
     LOG("[INIT] Motor shield intialised")
-      
+
+
+    /* initialise servos */
+    servoA.attach(5);
+    servoB.attach(6);
+    LOG("[INIT] Servos initialised");
+
+    /* initialise LEDs */
+    pinMode(4, OUTPUT);
+    pinMode(7, OUTPUT);
+    LOG("[INIT] LEDs initialised");
+    
     /* define callbacks for i2c */
     Wire.onReceive(receive_data_cb);
     Wire.onRequest(send_data_cb);
@@ -44,22 +59,7 @@ void setup() {
 
 /* main loop */
 void loop() {
-  /*
-        digitalWrite(12, 0); // A motor forward
-        digitalWrite(9, LOW); // disable A motor brake 
-        analogWrite(3, 100);
-
-        digitalWrite(13, 1); // B motor forward
-        digitalWrite(8, LOW); // disable A motor brake 
-        analogWrite(11, 100);
-
-        delay(500); // run motor for 200 microseconds
-
-        digitalWrite(9, HIGH); // enable A motor brake
-        digitalWrite(8, HIGH); // enable B motor brake
-
-        delay(2000);
-        */
+  
     //LOG("[LOOP] Starting loop")
     delay(100);
 }
@@ -76,7 +76,15 @@ void call_function(int fnum, int param1, int param2, int param3,int param4) {
               LOG("Calling function motor_move");
               motor_move(param1,param2,param3,param4);
               break;
-
+          case 2:
+              LOG("Calling servo_move");
+              servo_move(param1,param2);
+              break;
+          case 3:
+              LOG("Calling set_LEDs");
+              set_LEDs(param1,param2);
+              break;
+          
           default:
               LOG("Invalid function call");
               break;
@@ -84,17 +92,23 @@ void call_function(int fnum, int param1, int param2, int param3,int param4) {
     
 }
 
+void set_LEDs(int led1, int led2) {
+  
+        digitalWrite(4, led1);
+        digitalWrite(7, led2);
+}
+
 void motor_move(int A_dir, int B_dir, int A_speed, int B_speed) {
 
-        LOG("In the motor function");
         int n;
+        LOG("[MOTOR_MOVE] Motor is moving");
         for(n=0;n<100;n++) {
         digitalWrite(12, !A_dir); // A motor forward(weird wiring)
         digitalWrite(9, LOW); // disable A motor brake 
         analogWrite(3, A_speed);
 
-        digitalWrite(13, B_dir); // A motor forward
-        digitalWrite(8, LOW); // disable A motor brake 
+        digitalWrite(13, B_dir); // B motor forward
+        digitalWrite(8, LOW); // disable B motor brake 
         analogWrite(11, B_speed);
 
         delay(1000); // run motor for 200 microseconds
@@ -104,6 +118,22 @@ void motor_move(int A_dir, int B_dir, int A_speed, int B_speed) {
         }
 }
 
+void servo_move(int posA, int posB) {
+        if (posA >=10 && posA <=200 && posB <=90) {
+            LOG("[SERVO_MOVE] Servo moving into position");
+            LOG("[SERVO_MOVE] position of A");
+            LOG(posA);
+            LOG("[SERVO_MOVE] position of B");
+            LOG(posB);
+            servoA.write(posA); //60
+            servoB.write(posB); //90
+        }
+        else {
+            LOG("[SERVO_MOVE] Invalid parameters")
+        }
+        
+        
+}
 
 /* callback for received data */
 void receive_data_cb(int byteCount){
