fb817b28a9465e2299139db64c264c1815d75676
Daniel Hammer <braineniac@protonmail.com>
2018-11-28 10:33:01 +0100

    Headless arduino setup done

--- arduino/ctr_arduino/ctr_arduino.cpp
+++ arduino/ctr_arduino/ctr_arduino.cpp
@@ -1,3 +1,5 @@
+#include <Arduino.h>
+#include <SoftwareSerial.h>
 #include <Wire.h>
 #include <Servo.h>
 
@@ -15,6 +17,16 @@
 
 Servo servoA;
 Servo servoB;
+
+void call_function(int fnum, int param1, int param2, int param3,int param4);
+void set_LEDs(int led1, int led2);
+void motor_move(int A_dir, int B_dir, int A_speed, int B_speed);
+void servo_move(int posA, int posB);
+void receive_data_cb(int byteCount);
+void send_data_cb();
+
+
+
  
 void setup() {
 
