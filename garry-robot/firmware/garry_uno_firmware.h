6b032177be9bde76b506911b9a888b92cf2355d0
Daniel Hammer <braineniac@protonmail.com>
2018-12-07 13:45:44 +0100

    Arduino rosserial finally working

--- garry-robot/firmware/garry_uno_firmware.h
+++ garry-robot/firmware/garry_uno_firmware.h
@@ -1,3 +1,7 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 #include <Arduino.h>
 #include <Wire.h>
 #include <Servo.h>
@@ -8,16 +12,12 @@
 
 
 /* function definitions */
-void call_function(int fnum, int param1, int param2, int param3,int param4);
-void set_LEDs(int led1, int led2);
-void motor_move(int A_dir, int B_dir, int A_speed, int B_speed);
-void servo_move(int posA, int posB);
-void receive_data_cb(int byteCount);
-void send_data_cb();
-
 void led_left_eye_cb(const std_msgs::Bool& msg);
 void led_right_eye_cb(const std_msgs::Bool& msg);
+
 void servo_upper_cb(const geometry_msgs::Twist& msg);
 void servo_lower_cb(const geometry_msgs::Twist& msg);
+
 void motor_left_cb(const geometry_msgs::Twist& msg);
 void motor_right_cb(const geometry_msgs::Twist& msg);
+void motor_both_cb(const geometry_msgs::Twist& msg);
