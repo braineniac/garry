ae993a5ad3706c9c01e705f739636b2ef479989f
Daniel Hammer <braineniac@protonmail.com>
2019-02-19 13:00:28 +0000

    fixed services and rosinstall, added aliases, fixed wifi, added raspicam test

--- scripts/services.sh
+++ scripts/services.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 SYSTEMD=/etc/systemd/system
+SYSTMD_LIB=/lib/systemd/system
 SERVICES=/home/dan/ws_garry/garry/services
+
+rm -f $SYSTMD_LIB/dnsmasq.service
+sudo ln -s $SERVICES/dnsmasq.service $SYSTMD_LIB/dnsmasq.service
 
 rm -f $SYSTEMD/garry-eyes.service
 sudo ln -s $SERVICES/garry-eyes.service $SYSTEMD/garry-eyes.service 
